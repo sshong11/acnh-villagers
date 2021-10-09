683869f66c3014ed3a61b28032688e27df33be9e
sshong11 <sshong2016@gmail.com>
2021-10-09 12:59:07 -0400

    created villagers model

---
+++ models/villagers.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose")
+
+////////////////////////////////////////////////
+// Models
+////////////////////////////////////////////////
+const {Schema, model} = mongoose
+
+// make villagers schema
+const villagersSchema = new Schema({
+    name: String,
+    personality: String,
+    birthday_string: String,
+    species: String,
+    gender: String,
+    hobby: String,
+    icon_uri: String,
+    image_uri: String,
+    catch_phrase: String,
+    saying: String,
+})
+
+// make villagers model
+const Villager = model("Villager", villagersSchema)
+
+module.exports = Villager
