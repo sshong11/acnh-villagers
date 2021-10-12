6c1c77208e3cfa89824e528b14cc54e7f8286b2e
sshong11 <sshong2016@gmail.com>
2021-10-12 17:25:33 -0400

    created user signup routes

---
+++ controllers/user.js
@@ -0,0 +1,19 @@
+const express = require("express")
+const User = require("../models/user")
+const bcrypt = require("bcryptjs")
+const {application} = require("express")
+
+const router = express.Router()
+
+// SIGNUP
+router.get("/signup", (req, res) => {
+    res.render("user/signup.ejs")
+})
+
+router.post("/signup", (req, res) => {
+    req.body.password = await bcrypt.hash(req.body.password, await bcrypt.genSalt(10))
+
+    User.create(req.body, (err, user) => {
+        res.redirect("/user/login")
+    })
+})
--- utils/middleware.js
+++ utils/middleware.js
@@ -3,6 +3,7 @@ const express = require("express")
 const methodOverride = require("method-override")
 const morgan = require("morgan")
 const session = require("express-session")
+const MongoStore = require("connect-mongo")
 const VillagerRouter = require("../controllers/villagers")
 
 const middleware = (app) => {
@@ -10,12 +11,12 @@ const middleware = (app) => {
     app.use(methodOverride("_method")) // override for put and delete requests from forms
     app.use(express.urlencoded({extended: true})) // parse urlencoded request bodies
     app.use(express.static("public"))
-    // app.use(session({
-    //     secret: process.env.SECRET,
-    //     store: MongoStore.create({mongoUrl: process.env.DATABASE_URL}),
-    //     saveUninitialized: true,
-    //     resave: false,
-    // }))
+    app.use(session({
+        secret: process.env.SECRET,
+        store: MongoStore.create({mongoUrl: process.env.DATABASE_URL}),
+        saveUninitialized: true,
+        resave: false,
+    }))
     app.use("/villagers", VillagerRouter)
 }
 
